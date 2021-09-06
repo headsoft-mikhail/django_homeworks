0958ac53b850c811d9064a55e4e18b0b367557d1
Mikhail Afanasev <a.mike@bk.ru>
2021-09-06 19:33:58 +0300

    homework 1

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import os
+from datetime import datetime
 
 
 def home_view(request):
@@ -8,10 +10,10 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -21,15 +23,16 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
+    # возвращает текущее время
+    dt = datetime.now()
+    current_time = dt.strftime("%A, %d. %B %Y %H:%M:%S")
+    msg = f'Current time:<br>{current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
+    # возвращает список файлов в рабочей
     # директории
-    raise NotImplemented
+    files_list = 'Current directory:<br><ul type="square">' + \
+                 ''.join([f'<li>{item}</li>' for item in os.listdir()])
+    return HttpResponse(files_list)
