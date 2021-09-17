d9632acdb0922f3a21347fd5fab72d52bdf92fbb
Mikhail Afanasev <a.mike@bk.ru>
2021-09-17 21:40:29 +0300

    all problems with tags solved, program works as  requested

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -10,23 +10,12 @@ class ArticleTagsInlineFormset(BaseInlineFormSet):
         main_tag_exists = False
         for form in self.forms:
             articles_edit |= ('is_main' in form.cleaned_data.keys())
-            print(form.cleaned_data.get('is_main'))
             if form.cleaned_data.get('is_main'):
                 if main_tag_exists:
                     raise ValidationError('Основным должен быть только один Тэг')
                 main_tag_exists = True
         if not main_tag_exists and articles_edit:
             raise ValidationError('Хотя бы один Тэг должен быть основным')
-
-
-        # scope_ids = []
-        # for form in self.forms:
-        #     scope_id = form.cleaned_data.get('scope_id')
-        #     if scope_id is not None:
-        #         if scope_id not in scope_ids:
-        #             scope_ids.append(scope_id)
-        #         else:
-        #             raise ValidationError('Повторяющийся тэг')
 
         return super().clean()  # вызываем базовый код переопределяемого метода
 
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,24 +1,8 @@
 from django.db import models
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=256, verbose_name='Название')
-    text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-
-    class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
-        ordering = ['-published_at']
-
-    def __str__(self):
-        return self.title
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=20)
-    articles = models.ManyToManyField(Article, through='ArticleTags', related_name='scopes')
 
     def __str__(self):
         return self.name
@@ -29,12 +13,28 @@ class Tag(models.Model):
         ordering = ['name']
 
 
+class Article(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+    articles = models.ManyToManyField(Tag, through='ArticleTags')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+
 class ArticleTags(models.Model):
-    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
     is_main = models.BooleanField(default='False')
 
     class Meta:
-        ordering = ['-is_main']
+        ordering = ['-is_main', 'tag__name']
 
 
