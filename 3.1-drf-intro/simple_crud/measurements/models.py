f3f947b3eb9b5878220bf95bd688fa8334ee06f5
Mikhail Afanasev <a.mike@bk.ru>
2021-09-27 11:53:07 +0300

    first additional exercise done

--- 3.1-drf-intro/simple_crud/measurements/models.py
+++ 3.1-drf-intro/simple_crud/measurements/models.py
@@ -1,28 +1,24 @@
 from django.db import models
+# from django import forms
 
 
-class Project(models.Model):
+class CommonInfo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Project(CommonInfo):
     """Объект на котором проводят измерения."""
-
     name = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
 
 
-class Measurement(models.Model):
+class Measurement(CommonInfo):
     """Измерение температуры на объекте."""
-
     value = models.FloatField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
+    # image = forms.fields.ImageField(max_length=200, allow_empty_file=True)
